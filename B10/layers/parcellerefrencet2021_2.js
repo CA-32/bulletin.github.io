825957ecfe877bf7d6b2a00a47c6a670fdbb769b
CA-32 <68068347+CA-32@users.noreply.github.com>
2021-08-18 17:10:38 +0200

    Add files via upload

---
+++ B10/layers/parcellerefrencet2021_2.js
@@ -0,0 +1,22 @@
+var json_parcellerefrencet2021_2 = {
+"type": "FeatureCollection",
+"name": "parcellerefrencet2021_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "COMMUNE": "Sainte-marie", "COMMUNE0": "32388", "culture": "mais", "technicien": "RM", "zonage bul": "neste NE" }, "geometry": { "type": "Point", "coordinates": [ 0.867071907480864, 43.672159517415807 ] } },
+{ "type": "Feature", "properties": { "COMMUNE": "Luppe-violles", "COMMUNE0": "32220", "culture": "mais", "technicien": "TB", "zonage bul": "midour-douze" }, "geometry": { "type": "Point", "coordinates": [ -0.143240683776541, 43.735322482980834 ] } },
+{ "type": "Feature", "properties": { "COMMUNE": "Tirent-pontejac", "COMMUNE0": "32447", "culture": "mais", "technicien": "RM", "zonage bul": "neste S" }, "geometry": { "type": "Point", "coordinates": [ 0.793581355984705, 43.545949717011929 ] } },
+{ "type": "Feature", "properties": { "COMMUNE": "Beaucaire", "COMMUNE0": "32035", "culture": "mais", "technicien": "MG", "zonage bul": "neste N" }, "geometry": { "type": "Point", "coordinates": [ 0.37627016531355, 43.836847634754953 ] } },
+{ "type": "Feature", "properties": { "COMMUNE": "Castelnau-sur-l'auvignon", "COMMUNE0": "32080", "culture": "mais pop", "technicien": "MG", "zonage bul": "neste N" }, "geometry": { "type": "Point", "coordinates": [ 0.472026412437233, 43.963430429533496 ] } },
+{ "type": "Feature", "properties": { "COMMUNE": "Moncassin", "COMMUNE0": "32263", "culture": "mais", "technicien": "TB", "zonage bul": "neste S" }, "geometry": { "type": "Point", "coordinates": [ 0.474652754736062, 43.44925180967055 ] } },
+{ "type": "Feature", "properties": { "COMMUNE": "Sorbets", "COMMUNE0": "32437", "culture": "mais", "technicien": "TB", "zonage bul": "midour-douze" }, "geometry": { "type": "Point", "coordinates": [ -0.016167708126703, 43.71365868264634 ] } },
+{ "type": "Feature", "properties": { "COMMUNE": "Ceran", "COMMUNE0": "32101", "culture": "mais", "technicien": "RM", "zonage bul": "neste NE" }, "geometry": { "type": "Point", "coordinates": [ 0.698305728251847, 43.815165759431309 ] } },
+{ "type": "Feature", "properties": { "COMMUNE": "Gimont", "COMMUNE0": "32147", "culture": "soja", "technicien": "TB", "zonage bul": "neste NE" }, "geometry": { "type": "Point", "coordinates": [ 0.881749073017479, 43.611162710194129 ] } },
+{ "type": "Feature", "properties": { "COMMUNE": "Laree", "COMMUNE0": "32193", "culture": "mais sem", "technicien": "TB", "zonage bul": "midour-douze" }, "geometry": { "type": "Point", "coordinates": [ -0.05536999717151, 43.907407635772493 ] } },
+{ "type": "Feature", "properties": { "COMMUNE": "Miradoux", "COMMUNE0": "32253", "culture": "mais sem", "technicien": "MG", "zonage bul": "neste NE" }, "geometry": { "type": "Point", "coordinates": [ 0.758731711325818, 43.994693904827059 ] } },
+{ "type": "Feature", "properties": { "COMMUNE": "Riscle", "COMMUNE0": "32344", "culture": "mais", "technicien": "TB", "zonage bul": "adour" }, "geometry": { "type": "Point", "coordinates": [ -0.07826955891476, 43.645172832687983 ] } },
+{ "type": "Feature", "properties": { "COMMUNE": "Demu", "COMMUNE0": "32115", "culture": "mais", "technicien": "TB", "zonage bul": "midour-douze" }, "geometry": { "type": "Point", "coordinates": [ 0.136413365491709, 43.745440055871676 ] } },
+{ "type": "Feature", "properties": { "COMMUNE": "Barran", "COMMUNE0": "32029", "culture": "soja", "technicien": "TB", "zonage bul": "neste S" }, "geometry": { "type": "Point", "coordinates": [ 0.4485417001592, 43.622125650048972 ] } },
+{ "type": "Feature", "properties": { "COMMUNE": "Montadet", "COMMUNE0": "32276", "culture": "mais", "technicien": "RM", "zonage bul": "neste S" }, "geometry": { "type": "Point", "coordinates": [ 0.904604337628841, 43.435185562195514 ] } }
+]
+}
---
+++ B10/styles/zonagesbulletinsgers2021_1_style.js
@@ -0,0 +1,72 @@
+var size = 0;
+var placement = 'point';
+function categories_zonagesbulletinsgers2021_1(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                switch(value.toString()) {case 'Nord est':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.5)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(143,223,106,0.5)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Nord':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.5)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(69,216,128,0.5)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Sud':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.5)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(144,239,217,0.5)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Midouze':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.5)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(239,215,27,0.5)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Adour':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.5)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(253,174,97,0.5)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_zonagesbulletinsgers2021_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("nom secteu");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+var style = categories_zonagesbulletinsgers2021_1(feature, value, size, resolution, labelText,
+                          labelFont, labelFill, bufferColor,
+                          bufferWidth, placement);
+
+    return style;
+};
