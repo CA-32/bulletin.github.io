513e14a399573c2f891c8bf4aadf99a93ab1db64
CA-32 <68068347+CA-32@users.noreply.github.com>
2023-06-14 09:51:30 +0200

    Add files via upload

---
+++ data/CA32_2.js
@@ -0,0 +1,8 @@
+var json_CA32_2 = {
+"type": "FeatureCollection",
+"name": "CA32_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Retour": "https://gers.chambre-agriculture.fr/agroenvironnement/eau/le-pilotage-de-lirrigation/lamelioration-de-lefficience-de-lirrigation-a-la-parcelle/" }, "geometry": { "type": "Point", "coordinates": [ 0.528766515010164, 44.12582585004003 ] } }
+]
+}
